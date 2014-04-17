8a20423316239c3ae72fce2f8c488b9b7da84c63
Bjørn Sørensen <bsaren12@student.aau.dk>
2014-04-17 13:51:48 +0200

    Camera now follows the player and the jump action now acts on collision and is affected by rigidbody
    bugs: Jump acts on any collision, and must be fixed so it only works on one surface area of the map.

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : ObstacleBehavior {
+
+	public Transform target; //set position of the camera according to the player's placement (remember to assign the player object to target parameter in unity)
+	public float y = 1.0f;
+	public float z = -15.0f;
+	public float x = 1.0f;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = Vector3.Lerp(transform.position, new Vector3 (target.transform.position.x + x, target.transform.position.y + y, target.transform.position.z + z), 2.0f *Time.deltaTime); //enables the camera to follow the player according to his position and is updated every Time.deltaTime * 2.
+
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Player : CubbartController {
 	
+	private bool isFalling = false; // is a variable only the player needs, which checks whether player is in the air or not.
 
 	// Use this for initialization
 	void Start () {
@@ -10,27 +11,46 @@ public class Player : CubbartController {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+	
+		life = 1;
+		jump = 200;
+		speed = 4;
 
 		targetSpeed = Input.GetAxisRaw("Horizontal")*speed;
 
-		//Physics.gravity = Vector3(-1.0,0,0);
+		transform.position = new Vector3 (transform.position.x, transform.position.y, 0);// keeps player on the z-axis of virtual environment.
 
-		transform.position = new Vector3 (transform.position.x, transform.position.y, 0);
+		//___________________________________________________________________________________
 
 		transform.position = transform.position + new Vector3(speed,0,0) * Time.deltaTime;//automatic movement for the hero.
 		
+		//________________________________________________________________________________________
+
 		if(Input.GetKey(KeyCode.D)) //The possible movement of the hero
 			transform.position += new Vector3(0.2f,0,0);
 		
 		if(Input.GetKey(KeyCode.A))
 			transform.position -= new Vector3(0.2f,0,0);
 
-		if(Input.GetKey(KeyCode.S))
+		if(Input.GetKey(KeyCode.S)) // testing to see if player wont stray from the z-axis
 		transform.position += new Vector3(0,0,1);
 
-		if(Input.GetKey(KeyCode.Space))
-			transform.position += new Vector3(0,0.1f,0);
+		//_________________________________________________________________________________
 
+		if(Input.GetKey(KeyCode.Space)&& isFalling == false)// checks of space is being pressed and isFalling is false
+		{
+			rigidbody.AddForce (new Vector3(0,jump,0)); // travels the value of variable jump along the y-axis. Remember Addforce is affected by the property values of rigidbody
+		}
+		isFalling = true;// sets isFalling to true, so a jumps can't be made continously. Remember to place it outside of if, due to how Unity reads the code.
+	
+		//_______________________________________________________________________________
+	
+	}
+
+	void OnCollisionStay()
+	{
+		isFalling = false; // changes isFalling to false when in contact with an object
 	}
 }
