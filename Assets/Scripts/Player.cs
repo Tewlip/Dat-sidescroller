c36b55680d905bf0895ea2e62df3b198b01f52c0
Bjørn Sørensen <bsaren12@student.aau.dk>
2014-04-25 15:10:24 +0200

    Fixed stuff with jump mechanic.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,11 +3,7 @@ using System.Collections;
 
 public class Player : CubbartController {
 	
-
-	public static float distanceTraveled;
-
 	private bool isFalling = false; // is a variable only the player needs, which checks whether player is in the air or not.
-
 
 	// Use this for initialization
 	void Start () {
@@ -19,10 +15,8 @@ public class Player : CubbartController {
 	{
 	
 		life = 1;
-		jump = 200;
+		jump = 10;
 		speed = 4;
-
-		distanceTraveled = transform.localPosition.x; //update on the distance Player has traveled since start
 
 		targetSpeed = Input.GetAxisRaw("Horizontal")*speed;
 
@@ -31,8 +25,10 @@ public class Player : CubbartController {
 		//___________________________________________________________________________________
 
 		transform.position = transform.position + new Vector3(speed,0,0) * Time.deltaTime;//automatic movement for the hero.
-		
+
+
 		//________________________________________________________________________________________
+
 
 		if(Input.GetKey(KeyCode.D)) //The possible movement of the hero
 			transform.position += new Vector3(0.2f,0,0);
@@ -47,7 +43,7 @@ public class Player : CubbartController {
 
 		if(Input.GetKey(KeyCode.Space)&& isFalling == false)// checks of space is being pressed and isFalling is false
 		{
-			rigidbody.AddForce (new Vector3(0,jump,0)); // travels the value of variable jump along the y-axis. Remember Addforce is affected by the property values of rigidbody
+			rigidbody.velocity = (new Vector3(0,jump,0)); // travels the value of variable jump along the y-axis. Remember to use velocity as addforce can create some funky bugs with onCollision (velocity remains constant while addforce is += to the existing value).
 		}
 		isFalling = true;// sets isFalling to true, so a jumps can't be made continously. Remember to place it outside of if, due to how Unity reads the code.
 	
@@ -55,8 +51,27 @@ public class Player : CubbartController {
 	
 	}
 
-	void OnCollisionStay()
+	void OnCollisionStay(Collision interacter)
 	{
-		isFalling = false; // changes isFalling to false when in contact with an object
+		if(interacter.contacts.Length > 0){ // If the array length of interacter.contacts becomes greater than 0, do the following:
+			if(interacter.contacts[0].point.y < transform.position.y){// If the value of contact point is less than player's current position on the y-axis, do the following:
+				isFalling = false; // changes isFalling to false when in contact with an object
+			}
+		}
+		//______________________________________________________________________________
+	}
+
+	void OnCollisionEnter(Collision Player)
+	{
+
+		if(Player.gameObject.name == "enemy")
+		{
+			Destroy(gameObject);
+		}
+
+		if(Player.gameObject.name == "enemy" && Input.GetKey(KeyCode.G))
+		{
+
+		}
 	}
 }
