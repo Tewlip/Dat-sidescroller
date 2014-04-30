eb6707cc65240ac92ecc147875ac3226b8405252
Bjørn Sørensen <bsaren12@student.aau.dk>
2014-04-30 12:32:25 +0200

    some comments on booster

--- Assets/Scripts/Booster.cs
+++ Assets/Scripts/Booster.cs
@@ -7,16 +7,16 @@ public class Booster : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        gameObject.SetActive(false);
+        gameObject.SetActive(false); // Sets the designated gameobject to inactive.
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (transform.localPosition.x + recycleOffset < Player.distanceTraveled) {
+        if (transform.localPosition.x + recycleOffset < Player.distanceTraveled) { // Gameobject is inactive if its current position + recycleOffset value is less than player position
             gameObject.SetActive(false);
             return;
         }
-        transform.Rotate(rotationVelocity * Time.deltaTime);
+        transform.Rotate(rotationVelocity * Time.deltaTime); // Allows for rotation of the gameobject.
 	}
 
     public void SpawnIfAvailable(Vector3 position) {
@@ -27,7 +27,7 @@ public class Booster : MonoBehaviour {
         gameObject.SetActive(true);
     }
 
-    void OnTriggerEnter() {
+    void OnTriggerEnter() { // When player triggers gameobject, Addboost is called and gameobject is set to inactive.
         Player.AddBoost();
         gameObject.SetActive(false);
     }
